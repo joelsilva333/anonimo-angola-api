86ffa3aab957a3386d1eb39a731375d70ec212c8
Joel Silva <joelpitra44@gmail.com>
2025-08-20 14:21:54 +0100

    Remove dependências não utilizadas e ajusta configurações de importação no TypeScript para melhorar a estrutura do projeto.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import "module-alias/register";
 import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import "./database/connection";
+import "@/database/connection";
 import routes from "./routes";
-
-dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
