5f6dd09ae72b85806b7c85f5caf11fb7aadf9681
joelsilva333 <joelpitra44@gmail.com>
2025-08-20 21:57:08 +0100

    formata o código no index.ts para melhorar a legibilidade e a consistência

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-import "module-alias/register";
-import dotenv from "dotenv";
-dotenv.config();
+import "module-alias/register"
+import dotenv from "dotenv"
+dotenv.config()
 
-import express from "express";
-import cors from "cors";
-import "@/database/connection";
-import routes from "./routes";
+import express from "express"
+import cors from "cors"
+import "@/database/connection"
+import routes from "./routes"
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		credentials: true,
+	})
+)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(routes);
+app.use(routes)
 
 app.listen(PORT, () => {
-  console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
-});
+	console.log(`Swagger docs: http://localhost:${PORT}/api-docs`)
+})
