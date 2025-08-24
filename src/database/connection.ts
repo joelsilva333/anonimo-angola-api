989e6e21931ad32faf80972468d660c97ea13fd9
joelsilva333 <joelpitra44@gmail.com>
2025-08-24 10:10:41 +0100

    formata o código em connection.ts para melhorar a legibilidade e a consistência

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,41 +1,41 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
+import "reflect-metadata"
+import { DataSource } from "typeorm"
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production"
 
 const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432", 10),
-  username: process.env.DB_USER,
-  password: `${process.env.DB_PASSWORD}`,
-  database: process.env.DB_NAME,
-  logging: true,
-  synchronize: !isProd,
-  migrationsRun: true,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
-  entities: [
-    isProd
-      ? __dirname + "/../**/*.entity.js"
-      : __dirname + "/../**/*.entity.ts",
-  ],
-  migrations: [
-    isProd
-      ? __dirname + "/../migrations/*.js"
-      : __dirname + "/../migrations/*.ts",
-  ],
-  subscribers: [],
-});
+	type: "postgres",
+	host: process.env.DB_HOST,
+	port: parseInt(process.env.DB_PORT || "5432", 10),
+	username: process.env.DB_USER,
+	password: `${process.env.DB_PASSWORD}`,
+	database: process.env.DB_NAME,
+	logging: true,
+	synchronize: !isProd,
+	migrationsRun: true,
+	ssl:
+		process.env.NODE_ENV === "production"
+			? { rejectUnauthorized: false }
+			: false,
+	entities: [
+		isProd
+			? __dirname + "/../**/*.entity.js"
+			: __dirname + "/../**/*.entity.ts",
+	],
+	migrations: [
+		isProd
+			? __dirname + "/../migrations/*.js"
+			: __dirname + "/../migrations/*.ts",
+	],
+	subscribers: [],
+})
 
 AppDataSource.initialize()
-  .then(() => {
-    console.log("Conexão bem sucedida com o banco de dados");
-  })
-  .catch((err) => {
-    console.error("Erro ao conectar com o banco de dados", err);
-  });
+	.then(() => {
+		console.log("Conexão bem sucedida com o banco de dados")
+	})
+	.catch((err) => {
+		console.error("Erro ao conectar com o banco de dados", err)
+	})
 
-export default AppDataSource;
+export default AppDataSource
